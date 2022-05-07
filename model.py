bf81945339d978d74598f5cba3d5b63e3f0ee761
ZS <1002253858@qq.com>
2022-05-07 20:20:54 +0800

    add codes

---
+++ model.py
@@ -0,0 +1,179 @@
+import torch
+import torch.nn as nn
+import functools
+from torch.nn import init
+
+class netD(nn.Module):
+    def __init__(self, image_size=128, conv_dim=64, c_dim=3, repeat_num=5):
+        super(netD, self).__init__()
+        layers = []
+        layers.append(nn.Conv2d(2, conv_dim, kernel_size=3, stride=2, padding=1))
+        layers.append(nn.PReLU())
+
+        curr_dim = conv_dim
+        for i in range(1, repeat_num):
+            layers.append(nn.Conv2d(curr_dim, curr_dim * 2, kernel_size=3, stride=2, padding=1))
+            layers.append(nn.PReLU())
+            curr_dim *= 2
+        kernel_size = int(image_size / 2 ** repeat_num)
+        self.main = nn.Sequential(*layers)
+        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, a=0)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    nn.init.constant_(m.bias.data, 0.0)
+            if isinstance(m, nn.InstanceNorm2d) and m.bias is not None:
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x,y):
+        x = torch.cat([x, y], dim=1)
+        h = self.main(x)
+        out_src = self.conv1(h)
+        out_cls = self.conv2(h)
+        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+
+
+
+def get_norm_layer(norm_type='instance'):
+
+    if norm_type == 'batch':
+        norm_layer = functools.partial(nn.BatchNorm2d, affine=True, track_running_stats=True)
+    elif norm_type == 'instance':
+        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+    elif norm_type == 'group':
+        norm_layer = functools.partial(nn.GroupNorm, affine=False, track_running_stats=False)
+    elif norm_type == 'none':
+        norm_layer = None
+    else:
+        raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
+    return norm_layer
+
+
+
+def define_G(input_nc, output_nc, ngf, netG, norm='group', use_dropout=False, init_type='normal', init_gain=0.02,
+             gpu_ids=[]):
+
+    norm_layer = get_norm_layer(norm_type=norm)
+
+    if netG == 'unet':
+        net = UnetGenerator(input_nc, output_nc, 7, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
+    else:
+        raise NotImplementedError('Generator model name [%s] is not recognized' % netG)
+    return init_net(net, init_type, init_gain)
+
+
+
+
+class UnetGenerator(nn.Module):
+
+    def __init__(self, input_nc, output_nc, num_downs, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False):
+
+        super(UnetGenerator, self).__init__()
+        unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=None, norm_layer=norm_layer,
+                                             innermost=True)
+        for i in range(num_downs - 5):
+            unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block,
+                                                 norm_layer=norm_layer, use_dropout=use_dropout)
+        unet_block = UnetSkipConnectionBlock(ngf * 4, ngf * 8, input_nc=None, submodule=unet_block,
+                                             norm_layer=norm_layer)
+        unet_block = UnetSkipConnectionBlock(ngf * 2, ngf * 4, input_nc=None, submodule=unet_block,
+                                             norm_layer=norm_layer)
+        unet_block = UnetSkipConnectionBlock(ngf, ngf * 2, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
+        self.model = UnetSkipConnectionBlock(output_nc, ngf, input_nc=input_nc, submodule=unet_block, outermost=True,
+                                             norm_layer=norm_layer)
+
+    def forward(self, x, c):
+        c = c.view(c.size(0), c.size(1), 1, 1)
+        c = c.repeat(1, 1, x.size(2), x.size(3))
+        x = torch.cat([x, c], dim=1)
+        return self.model(x)
+
+
+def init_weights(net, init_type='kaiming', init_gain=0.02):
+
+
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+        elif classname.find(
+                'BatchNorm2d') != -1:
+            init.normal_(m.weight.data, 1.0, init_gain)
+            init.constant_(m.bias.data, 0.0)
+
+    print('initialize network with %s' % init_type)
+    net.apply(init_func)
+
+
+def init_net(net, init_type='normal', init_gain=0.02):
+    init_weights(net, init_type, init_gain=init_gain)
+    return net
+
+
+class UnetSkipConnectionBlock(nn.Module):
+
+    def __init__(self, outer_nc, inner_nc, input_nc=None,
+                 submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
+
+        super(UnetSkipConnectionBlock, self).__init__()
+        self.outermost = outermost
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+        if input_nc is None:
+            input_nc = outer_nc
+        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4,
+                             stride=2, padding=1, bias=use_bias)
+        downrelu = nn.LeakyReLU(0.2, True)
+        downnorm = norm_layer(inner_nc)
+        uprelu = nn.PReLU()
+        upnorm = norm_layer(outer_nc)
+
+        if outermost:
+            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc,
+                                        kernel_size=4, stride=2,
+                                        padding=1)
+            down = [downconv]
+            up = [uprelu, upconv, nn.Tanh()]
+            model = down + [submodule] + up
+        elif innermost:
+            upconv = nn.ConvTranspose2d(inner_nc, outer_nc,
+                                        kernel_size=4, stride=2,
+                                        padding=1, bias=use_bias)
+            down = [downrelu, downconv]
+            up = [uprelu, upconv, upnorm]
+            model = down + up
+        else:
+            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc,
+                                        kernel_size=4, stride=2,
+                                        padding=1, bias=use_bias)
+            down = [downrelu, downconv, downnorm]
+            up = [uprelu, upconv, upnorm]
+
+            if use_dropout:
+                model = down + [submodule] + up + [nn.Dropout(0.5)]
+            else:
+                model = down + [submodule] + up
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        if self.outermost:
+            return self.model(x)
+        else:
+            return torch.cat([x, self.model(x)], 1)
---
+++ train_gan.py
@@ -0,0 +1,200 @@
+import torch
+from torch.utils import data
+from dataset_brain import Dataset_gan
+from model import netD, define_G
+from utils import label2onehot, classification_loss, gradient_penalty, seed_torch, update_lr
+import numpy as np
+import time
+import matplotlib as mpl
+import os
+import matplotlib.pyplot as plt
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+mpl.use('Agg')
+
+seed_torch()
+
+print('*******************train_gan*******************')
+file_path = './brats_npy/npy_gan/gan_t1.npy'
+train_data = Dataset_gan(file_path)
+batch_size = 128
+
+############fixed img
+test_data = Dataset_gan('./brats_npy/npy_train/train_t1.npy')
+fix_loader = data.DataLoader(dataset=test_data, batch_size=1, num_workers=4)
+fix_iter = iter(fix_loader)
+for i in range(40):
+    next(fix_iter)
+flair_fix, t1_fix, t1ce_fix, t2_fix, seg_fix = next(fix_iter)
+origin_fix = np.hstack((t2_fix[0][0], flair_fix[0][0], t1ce_fix[0][0], t1_fix[0][0], seg_fix[0][0]))
+for i in range(140):
+    next(fix_iter)
+flair_fix_2, t1_fix_2, t1ce_fix_2, t2_fix_2, seg_fix_2 = next(fix_iter)
+origin_fix_2 = np.hstack((t2_fix_2[0][0], flair_fix_2[0][0], t1ce_fix_2[0][0], t1_fix_2[0][0], seg_fix_2[0][0]))
+for i in range(100):
+    next(fix_iter)
+flair_fix_3, t1_fix_3, t1ce_fix_3, t2_fix_3, seg_fix_3 = next(fix_iter)
+origin_fix_3 = np.hstack((t2_fix_3[0][0], flair_fix_3[0][0], t1ce_fix_3[0][0], t1_fix_3[0][0], seg_fix_3[0][0]))
+print('------------------finsh fixed image------------------')
+del fix_loader, fix_iter, test_data
+############
+
+train_loader = data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=4)
+
+generator = define_G(4, 1, 64, 'unet', norm='instance', )
+discriminator = netD()
+
+optimizer_g = torch.optim.Adam(generator.parameters(), lr=0.0002)
+optimizer_d = torch.optim.Adam(discriminator.parameters(), lr=0.0002)
+
+generator.cuda()
+discriminator.cuda()
+EPOCH = 100
+num_iter = len(train_loader)
+D_LOSS = []
+G_LOSS = []
+f = open("./loss_gan.txt", 'a')
+print(time.strftime('|---------%Y-%m-%d   %H:%M:%S---------|', time.localtime(time.time())), file=f)
+discriminator.train()
+
+LAMBDA_CLS = 20
+LAMBDA_REC = 0.1
+LAMBDA_GP = 100
+
+for epoch in range(EPOCH):
+    if epoch == 30:
+        update_lr(optimizer_g, 0.0001)
+        update_lr(optimizer_d, 0.0001)
+        print('change lr to :', optimizer_g.param_groups[0]['lr'])
+    elif epoch == 60:
+        update_lr(optimizer_g, 0.00005)
+        update_lr(optimizer_d, 0.00005)
+        print('change lr to :', optimizer_g.param_groups[0]['lr'])
+    elif epoch == 90:
+        update_lr(optimizer_g, 0.00001)
+        update_lr(optimizer_d, 0.00001)
+        print('change lr to :', optimizer_g.param_groups[0]['lr'])
+    d_loss_ = 0
+    g_loss_ = 0
+    g_loss_t2_ = 0
+    g_loss_total_ = 0
+    d_loss_real_ = 0
+    d_loss_cls_ = 0
+    d_loss_fake_ = 0
+    d_loss_gp_ = 0
+    g_loss_fake_ = 0
+    g_loss_cls_ = 0
+    g_loss_rec_ = 0
+    g_loss_fake_t2_ = 0
+    g_loss_cls_t2_ = 0
+    g_loss_rec_t2_ = 0
+    generator.train()
+    for i, (flair, t1, t1ce, t2, seg) in enumerate(train_loader):
+        ####real
+        label_ = torch.randint(3, (t1.size(0),))
+        label = label2onehot(label_, 3).cuda()
+        real = torch.zeros(t1.size(0), t1.size(1), t1.size(2), t1.size(3))
+        for i, l in enumerate(label_):
+            if l == 0:
+                real[i] = flair[i]
+            elif l == 1:
+                real[i] = t1ce[i]
+            elif l == 2:
+                real[i] = t1[i]
+            else:
+                print('erro!!!')
+        out_src, out_cls = discriminator(real.float().cuda(), t2.float().cuda())
+        d_loss_real = - torch.mean(out_src.sum([1, 2, 3]))
+        d_loss_cls = classification_loss(out_cls, label)
+
+        #### discriminator
+        fake = generator(t2.float().cuda(), label)
+        out_src, out_cls = discriminator(fake.detach(), t2.float().cuda())
+        d_loss_fake = torch.mean(out_src.sum([1, 2, 3]))
+
+        #### Compute loss for gradient penalty.
+        alpha = torch.rand(real.size(0), 1, 1, 1).cuda()
+        x_hat = (alpha * real.cuda().data + (1 - alpha) * fake.data).requires_grad_(True)
+        out_src, _ = discriminator(x_hat, t2.float().cuda())
+        d_loss_gp = gradient_penalty(out_src, x_hat)
+        d_loss =  d_loss_real +  d_loss_fake + LAMBDA_CLS * d_loss_cls + LAMBDA_GP * d_loss_gp
+        optimizer_d.zero_grad()
+        d_loss.backward()
+        optimizer_d.step()
+
+        #### generator
+        fake = generator(t2.float().cuda(), label)
+        out_src, out_cls = discriminator(fake, t2.float().cuda())
+        g_loss_fake = -torch.mean(out_src.sum([1, 2, 3]))
+        g_loss_cls = classification_loss(out_cls, label)
+        g_loss_rec = torch.mean(torch.abs(real.float().cuda() - fake).sum([1, 2, 3]))
+
+        ####MMtr
+        fake = generator(t2.float().cuda(), label)
+        fake_t2 = generator(fake.float().cuda(), label)
+        out_src_t2, out_cls_t2 = discriminator(fake_t2, t2.float().cuda())
+        g_loss_fake_t2 = -torch.mean(out_src_t2.sum([1, 2, 3]))
+        g_loss_cls_t2 = classification_loss(out_cls_t2, label)
+        g_loss_rec_t2 = torch.mean(torch.abs(fake.float().cuda() - fake_t2).sum([1, 2, 3]))
+
+        g_loss =  g_loss_fake + LAMBDA_CLS * g_loss_cls + LAMBDA_REC * g_loss_rec
+        g_loss_t2 =  g_loss_fake_t2 + LAMBDA_CLS * g_loss_cls_t2 + LAMBDA_REC * g_loss_rec_t2
+
+        g_loss_total = g_loss + g_loss_t2
+        optimizer_g.zero_grad()
+        g_loss_total.backward()
+        optimizer_g.step()
+
+
+        d_loss_ += d_loss.data.cuda()
+        g_loss_total_ += g_loss_total.data.cuda()
+
+        d_loss_real_ += d_loss_real.data.cuda()
+        d_loss_cls_ += d_loss_cls.data.cuda()
+        d_loss_fake_ += d_loss_fake.data.cuda()
+        d_loss_gp_ += d_loss_gp.data.cuda()
+
+        g_loss_fake_ += g_loss_fake.data.cuda()
+        g_loss_cls_ += g_loss_cls.data.cuda()
+        g_loss_rec_ += g_loss_rec.data.cuda()
+
+        g_loss_fake_t2_ += g_loss_fake_t2.data.cuda()
+        g_loss_cls_t2_ += g_loss_cls_t2.data.cuda()
+        g_loss_rec_t2_ += g_loss_rec_t2.data.cuda()
+
+
+
+    print('EPOCH %d : d_loss = %.4f , g_loss_total = %.4f ' % (
+    epoch, d_loss_ / num_iter, g_loss_total_ / num_iter))
+    print(
+        " d_real = %.4f , d_fake = %.4f , d_cls = %.4f , d_gp = %.4f | \n "
+        "g_fake = %.4f , g_cls = %.4f , g_rec = %.4f | \n "
+        "g_fake_t2 = %.4f , g_cls_t2 = %.4f , g_rec_t2 = %.4f" % (
+        d_loss_real_ / num_iter, d_loss_fake_ / num_iter, d_loss_cls_ / num_iter, d_loss_gp.data.cuda(),
+        g_loss_fake_ / num_iter, g_loss_cls_ / num_iter, g_loss_rec_ / num_iter,
+        g_loss_fake_t2_ / num_iter, g_loss_cls_t2_ / num_iter, g_loss_rec_t2_ / num_iter))
+    print(
+        "EPOCH %d : d_loss = %.4f , d_real = %.4f , d_fake = %.4f , d_cls = %.4f , d_gp = %.4f | \n"
+        "g_loss = %.4f , g_fake = %.4f , g_cls = %.4f , g_rec = %.4f | \n "
+        "g_fake_t2 = %.4f , g_cls_t2 = %.4f , g_rec_t2 = %.4f" % (
+        epoch, d_loss_ / num_iter, d_loss_real_ / num_iter, d_loss_fake_ / num_iter, d_loss_cls_ / num_iter, d_loss_gp.data.cuda(),
+        g_loss_ / num_iter, g_loss_fake_ / num_iter, g_loss_cls_ / num_iter, g_loss_rec_ / num_iter,
+        g_loss_fake_t2_ / num_iter, g_loss_cls_t2_ / num_iter, g_loss_rec_t2_ / num_iter), file=f) # , g_seg = %.4f , g_loss_seg_ / num_iter
+    D_LOSS.append(d_loss_ / num_iter)
+    G_LOSS.append(g_loss_total_/ num_iter)
+    x = [i for i in range(epoch + 1)]
+    plt.plot(x, G_LOSS, label='generator')
+    plt.plot(x, D_LOSS, label='discriminator')
+    print('---------------loss plot----------------')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('gan_bw.png', format='png')
+    plt.close()
+
+
+f.close()
+
+model_save_g = './weight/generator_t2_tumor_bw.pth'
+model_save_d = './weight/discriminator_t2_bw.pth'
+torch.save(generator.state_dict(), model_save_g)
+torch.save(discriminator.state_dict(), model_save_d)
