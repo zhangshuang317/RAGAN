a3fd66c21d7c9f4145faa8f5b8a593834d3489ae
ZS <54140539+zhangshuang317@users.noreply.github.com>
2022-05-25 13:21:26 +0800

    Update npy2png.py

--- npy2png.py
+++ npy2png.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy.misc
-import os
 import cv2 as cv
 
 con_arr = np.load("./brats_npy/npy_pred/pred_t1ce.npy")
@@ -13,6 +10,5 @@ for i in range(0,10685):
         for n in range(arr.shape[1]):
             arr[m,n] = (arr[m,n] - mmin) *255/ (mmax-mmin)
     disp_to_img = cv.resize(arr,(240,240))
-    # cv.imshow(disp_to_img)
     cv.imwrite("./brats_npy/t1ce_png1/%d.png"%i,disp_to_img)
     print(i)
